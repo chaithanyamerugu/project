4b1f98cdbb0c23c8085f82bf22b98985be2bb069
chaithanyamerugu <97839284+chaithanyamerugu@users.noreply.github.com>
2022-01-16 21:09:58 +0530

    Add files via upload

---
+++ Human.java
@@ -0,0 +1,19 @@
+package methods;
+
+public class Human {
+public static void main(String[] args) {
+		walk();
+		talk();
+		breathe();
+	}
+	static void walk() {
+		System.out.println("human can walk...");
+	}
+	static void talk() {
+		System.out.println("human can speak...");
+	}
+	static void breathe() {
+		System.out.println("human can breathe...");
+	}
+
+}
